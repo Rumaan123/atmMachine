831eb0f9e3674959646071e94d7206dd48447332
Rumaan Iqbal <161339664+Rumaan123@users.noreply.github.com>
2024-05-15 22:40:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer_1.default.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+//   12345 === 1234 - false
+if (pinAnswer.pin === myPin) {
+    console.log("correct your pin!!!!");
+    let operationAns = await inquirer_1.default.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer_1.default.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        // =, -=, +=
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is:" + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance in" + myBalance);
+    }
+}
+else {
+    console.log("incorrect pin code!!!");
+}
